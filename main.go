8514bc1c841ff1ea8dcd1c9f19b047d2f5528d25
hhhtylerw <hhhtylerw@gmail.com>
2022-01-29 04:03:23 -0500

    rearrange files

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./wordle")))
+	http.Handle("/", http.FileServer(http.Dir("wordle-suite")))
 	http.ListenAndServe(":3000", nil)
 }
