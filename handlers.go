dffb06db14a0f6f8bc8186cb202fa6f4b08b3195
hhhtylerw <hhhtylerw@gmail.com>
2022-01-30 01:31:52 -0500

    styling

--- handlers.go
+++ handlers.go
@@ -1,10 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func SignUp(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	// Get username and password from request
+	r.ParseForm()
+	username := r.Form.Get("username")
+	password := r.Form.Get("password")
+
+	// Verify username is available
+	if !usernameAvailable(username) {
+		w.Write([]byte("Username taken"))
+		return
+	}
+
+	// Create new user account
+	client.Collection("accounts").Doc(username).Set(ctx, map[string]interface{}{
+		"username": username,
+		"password": getSHA256(password + salt),
+		"friends":  []string{"h3x"},
+		"plays":    0,
+		"wins":     0,
+		"losses":   0,
+	})
+	w.Header().Set("Session", username)
+	w.Write([]byte("Success"))
 }
+
+func usernameAvailable(username string) bool { // Check if username exists in database
+	userAccount, err := client.Collection("accounts").Doc(username).Get(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return true
+	}
+	return !userAccount.Exists()
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
+
 	w.Write([]byte("Hello, World!"))
 }
--- main.go
+++ main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"net/http"
 	"os"
+
+	"cloud.google.com/go/firestore"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
 )
 
-func main() {
-	port := os.Getenv("PORT")
+var (
+	ctx    context.Context = context.Background()
+	client *firestore.Client
+	salt   string
+)
 
+func init() {
+	godotenv.Load()
+	salt = os.Getenv("SALT")
+}
+
+func main() {
+	// Initialize Firestore client
+	var err error
+	opt := option.WithCredentialsFile("wordle-suite-firebase-adminsdk-n1yxj-ed63a51e95.json")
+	client, err = firestore.NewClient(ctx, "wordle-suite", opt)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Close()
+
+	// Initialize HTTP server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.Handle("/", http.FileServer(http.Dir("wordle-suite")))
 	http.HandleFunc("/signup", SignUp)
 	http.HandleFunc("/login", Login)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func getSHA256(input string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(input))
+	output := hex.EncodeToString(hasher.Sum(nil))
+	return string(output)
 }
