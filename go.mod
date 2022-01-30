d91c5dedab2236281702b49f6354f56c65989936
hhhtylerw <hhhtylerw@gmail.com>
2022-01-29 19:36:05 -0500

    func handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module wordle-suite
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -9,5 +9,6 @@ func main() {
 	port := os.Getenv("PORT")
 
 	http.Handle("/", http.FileServer(http.Dir("wordle-suite")))
+	http.HandleFunc("/test", One)
 	http.ListenAndServe(":"+port, nil)
 }
